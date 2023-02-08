a433e8f49fdb3938969caa693b469ea24be33342
Mikel <mikelcobian@gmail.com>
2023-02-08 17:59:39 +0100

    test(UColor): testCreation covered

--- Tests/UColorTests/UColorTests.swift
+++ Tests/UColorTests/UColorTests.swift
@@ -3,6 +3,7 @@ import XCTest
 import SwiftUI
 @testable import UColor
 
+
 class UColorTests: XCTestCase {
 	func testHex() {
 		let color = UColor(rgba: "#9933ccff")
@@ -14,4 +15,14 @@ class UColorTests: XCTestCase {
 		let invalid = UColor(rgba: "#hello")
 		XCTAssertNil(invalid)
 	}
+	
+	func testCreation() {
+		XCTAssertEqual(UColor.white, UColor(red: 1, green: 1, blue: 1))
+		XCTAssertEqual(UColor.black, UColor(red: 0, green: 0, blue: 0))
+		XCTAssertEqual(UColor.red, UColor(red: 1, green: 0, blue: 0))
+		XCTAssertEqual(UColor.green, UColor(red: 0, green: 1, blue: 0))
+		XCTAssertEqual(UColor.blue, UColor(red: 0, green: 0, blue: 1))
+	}
+	
+	
 }
